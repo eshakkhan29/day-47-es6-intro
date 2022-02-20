bff07f956f1e34f7eeccf7693e9daa2fa28933d3
eshakkhan29 <eshakkhan29@gmail.com>
2022-02-20 11:41:40 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// function getSum(a, b=9){return a+b;} 
+// console.log(getSum(2, 7));
+
+// const y = x => x*x; 
+// const z = y(5); 
+// console.log(z);
+
+myFunc =() => console.log('lol');
+
+// const numbers=[10,20,30,40,50,60,70]
+// console.log(Math.max(...numbers));
+
+// const cars=['toyota', 'honda','mercedes']; 
+// const newCars= [...cars , 'ferrari'];
+// console.log(newCars);
